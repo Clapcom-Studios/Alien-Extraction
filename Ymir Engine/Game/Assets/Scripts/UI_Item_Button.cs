9ae52a845c72e8c9a13a1f350cfcc7092192c3f8
AndyCubico <99950309+AndyCubico@users.noreply.github.com>
2024-06-01 13:37:25 +0200

    Update UI_Item_Button.cs

--- Ymir Engine/Game/Assets/Scripts/UI_Item_Button.cs
+++ Ymir Engine/Game/Assets/Scripts/UI_Item_Button.cs
@@ -59,19 +59,22 @@ public class UI_Item_Button : YmirComponent
 
     public void Update()
     {
-        if (updateStats && _menuReference.GetComponent<UI_Inventory>() != null && Equals(menuName, "Inventory Menu"))
+        if (updateStats)
         {
             if (item.currentSlot != ITEM_SLOT.NONE && item.currentSlot != ITEM_SLOT.SAVE)
             {
                 item.inSave = false;
 
-                if (!item.isEquipped)
+                if (_menuReference.GetComponent<UI_Inventory>() != null && Equals(menuName, "Inventory Menu"))
                 {
-                    item.isEquipped = true;
-                    item.inInventory = true;
+                    if (!item.isEquipped)
+                    {
+                        item.isEquipped = true;
+                        item.inInventory = true;
 
-                    item.UpdateStats();
-                    _menuReference.GetComponent<UI_Inventory>().UpdateTextStats();
+                        item.UpdateStats();
+                        _menuReference.GetComponent<UI_Inventory>().UpdateTextStats();
+                    }
                 }
             }
             else /*if (item.currentSlot != ITEM_SLOT.SAVE)*/
@@ -86,16 +89,18 @@ public class UI_Item_Button : YmirComponent
                     item.inInventory = false;
                 }
 
-                if (item.isEquipped)
+                if (_menuReference.GetComponent<UI_Inventory>() != null && Equals(menuName, "Inventory Menu"))
                 {
-                    item.isEquipped = false;
-                    item.inInventory = false;
+                    if (item.isEquipped)
+                    {
+                        item.isEquipped = false;
+                        item.inInventory = false;
 
-                    item.UpdateStats();
-                    _menuReference.GetComponent<UI_Inventory>().UpdateTextStats();
+                        item.UpdateStats();
+                        _menuReference.GetComponent<UI_Inventory>().UpdateTextStats();
+                    }
                 }
             }
-
             updateStats = false;
         }
 
@@ -268,8 +273,8 @@ public class UI_Item_Button : YmirComponent
             /*{*/(((!_item.isEquipped && !Equals(menuName, "Inventory Menu")) ||
             ((_item.isEquipped && _item.itemType == item.currentSlot ||
             item.currentSlot == ITEM_SLOT.NONE ||
-            item.currentSlot == ITEM_SLOT.MATERIAL) 
-            
+            item.currentSlot == ITEM_SLOT.MATERIAL)
+
             && Equals(menuName, "Inventory Menu"))) && !_item.inSave) || /*}*/
 
             item.currentSlot == ITEM_SLOT.SAVE && _item.inSave)
