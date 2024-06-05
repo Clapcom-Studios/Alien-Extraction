e096fe2f67f715e265ec90386879fac71ab254e2
Víctor Gil Espallargas <victorgile14@gmail.com>
2024-06-05 19:57:53 +0200

    Ambient sounds + trampa

--- Ymir Engine/Game/Assets/SoundBanks/Wwise_IDs.h
+++ Ymir Engine/Game/Assets/SoundBanks/Wwise_IDs.h
@@ -13,17 +13,15 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID ASHES = 3157117959U;
         static const AkUniqueID B_AIRCOND = 3898569340U;
-        static const AkUniqueID B_BASIC = 3279702346U;
         static const AkUniqueID B_CABLESSPURT = 3589440314U;
-        static const AkUniqueID B_CAIUS = 1712983321U;
         static const AkUniqueID B_CLOSEDOOR = 3305540428U;
         static const AkUniqueID B_CONSOLES = 1321898274U;
-        static const AkUniqueID B_DEPARTING = 1841822454U;
-        static const AkUniqueID B_METAL = 1253762097U;
         static const AkUniqueID B_MUSIC = 2462702937U;
         static const AkUniqueID B_OPENDOOR = 1237118816U;
         static const AkUniqueID B_TELEPORT = 3258831073U;
+        static const AkUniqueID BONE = 1744723399U;
         static const AkUniqueID C_CONFIRM = 4124650545U;
         static const AkUniqueID C_TALK = 1343835049U;
         static const AkUniqueID DX_CLAW = 3945193905U;
@@ -35,19 +33,17 @@ namespace AK
         static const AkUniqueID FH_DEATH = 1939824144U;
         static const AkUniqueID FH_MOVE = 2148321349U;
         static const AkUniqueID FH_TAIL = 2363156028U;
+        static const AkUniqueID FLESH = 1153642577U;
+        static const AkUniqueID HALLWAY = 2198133169U;
         static const AkUniqueID I_TALK = 836712503U;
-        static const AkUniqueID LV1_ASHES = 3867000129U;
         static const AkUniqueID LV1_BEACON = 60316943U;
         static const AkUniqueID LV1_CABLESSPURT = 2482357211U;
         static const AkUniqueID LV1_CLOSEDOOR = 2671563405U;
         static const AkUniqueID LV1_COLLECTITEM = 2518039338U;
-        static const AkUniqueID LV1_COMBAT = 559267303U;
         static const AkUniqueID LV1_FIRE = 2203822039U;
         static const AkUniqueID LV1_GRABKEY = 1939003214U;
-        static const AkUniqueID LV1_GRABRESIN = 1988943920U;
         static const AkUniqueID LV1_LEAKPIPES = 3849438701U;
         static const AkUniqueID LV1_MACHINERY = 3137796001U;
-        static const AkUniqueID LV1_METAL = 1423113456U;
         static const AkUniqueID LV1_OPENCHEST = 2450396122U;
         static const AkUniqueID LV1_OPENDOOR = 346664603U;
         static const AkUniqueID LV1_USEKEY = 3616258525U;
@@ -56,37 +52,29 @@ namespace AK
         static const AkUniqueID LV2_CABLESSPURT = 3705695092U;
         static const AkUniqueID LV2_CLOSEDOOR = 841889754U;
         static const AkUniqueID LV2_COLLECTITEM = 2080472109U;
-        static const AkUniqueID LV2_COMBAT = 3491575422U;
         static const AkUniqueID LV2_CONSOLES = 3677345244U;
+        static const AkUniqueID LV2_ELECTRICT = 684578341U;
         static const AkUniqueID LV2_GRABKEY = 936921561U;
-        static const AkUniqueID LV2_GRABRESIN = 3715372583U;
-        static const AkUniqueID LV2_INTER = 3026565220U;
-        static const AkUniqueID LV2_METAL = 2946420807U;
         static const AkUniqueID LV2_OPENCHEST = 442070525U;
         static const AkUniqueID LV2_OPENDOOR = 2558715662U;
         static const AkUniqueID LV2_REACTOR = 481681728U;
         static const AkUniqueID LV2_SIREN = 890091745U;
         static const AkUniqueID LV2_USEKEY = 1928507640U;
-        static const AkUniqueID LV3_BASIC = 2102085613U;
         static const AkUniqueID LV3_BEACON = 1880536425U;
-        static const AkUniqueID LV3_BOSS = 3961795458U;
+        static const AkUniqueID LV3_BOSSFIGHT = 1549769712U;
         static const AkUniqueID LV3_CLOSEDOOR = 1633402355U;
         static const AkUniqueID LV3_COLLECTITEM = 269349380U;
-        static const AkUniqueID LV3_COMBAT = 1402924317U;
         static const AkUniqueID LV3_DISTALIENCRY = 3203011774U;
-        static const AkUniqueID LV3_FLESH = 3785941297U;
         static const AkUniqueID LV3_GRABKEY = 2705516844U;
-        static const AkUniqueID LV3_GRABRESIN = 2758019366U;
         static const AkUniqueID LV3_HEARTBEATS = 2317714342U;
         static const AkUniqueID LV3_OPENCHEST = 861924392U;
         static const AkUniqueID LV3_OPENDOOR = 2654175985U;
-        static const AkUniqueID LV3_ORGANS = 2040334075U;
-        static const AkUniqueID LV3_STONE = 4273619196U;
         static const AkUniqueID LV3_USEKEY = 3605471807U;
         static const AkUniqueID LV3_VEINS = 2858739430U;
         static const AkUniqueID LVL1_MUSIC = 266958530U;
         static const AkUniqueID LVL2_MUSIC = 886520485U;
         static const AkUniqueID LVL3_MUSIC = 761846308U;
+        static const AkUniqueID METAL = 2473969246U;
         static const AkUniqueID P_ACIDSPIT = 4041423483U;
         static const AkUniqueID P_ACIDSPITHIT = 1348116050U;
         static const AkUniqueID P_ACIDSPITPUDDLE = 2936341071U;
@@ -116,8 +104,8 @@ namespace AK
         static const AkUniqueID UI_EQUIPCHIP = 919962612U;
         static const AkUniqueID UI_EQUIPCONSUMABLE = 4038071479U;
         static const AkUniqueID UI_ERROR = 1009189048U;
-        static const AkUniqueID UI_MENU = 2511555531U;
         static const AkUniqueID UI_MOVEHOVER = 3300279817U;
+        static const AkUniqueID UI_MUSIC = 743991111U;
         static const AkUniqueID UI_OPENINVENTORY = 1157348174U;
         static const AkUniqueID UI_OPENMAP = 1187363912U;
         static const AkUniqueID UI_OPENMENU = 2672940587U;
@@ -192,20 +180,26 @@ namespace AK
 
     } // namespace STATES
 
+    namespace SWITCHES
+    {
+        namespace MATERIALS
+        {
+            static const AkUniqueID GROUP = 4050929301U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ASHES = 3157117959U;
+                static const AkUniqueID BONE = 1744723399U;
+                static const AkUniqueID FLESH = 1153642577U;
+                static const AkUniqueID METAL = 2473969246U;
+            } // namespace SWITCH
+        } // namespace MATERIALS
+
+    } // namespace SWITCHES
+
     namespace GAME_PARAMETERS
     {
-        static const AkUniqueID PLAYBACK_RATE = 1524500807U;
-        static const AkUniqueID RPM = 796049864U;
-        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
-        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
-        static const AkUniqueID SS_AIR_FURY = 1029930033U;
-        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
-        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
-        static const AkUniqueID SS_AIR_RPM = 822163944U;
-        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
-        static const AkUniqueID SS_AIR_STORM = 3715662592U;
-        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
-        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+        static const AkUniqueID PLAYERHEALTH = 151362964U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
@@ -230,6 +224,7 @@ namespace AK
         static const AkUniqueID MAPINTERACTIONLV1 = 1430525662U;
         static const AkUniqueID MAPINTERACTIONLV2 = 1430525661U;
         static const AkUniqueID MAPINTERACTIONLV3 = 1430525660U;
+        static const AkUniqueID MATERIALS = 4050929301U;
         static const AkUniqueID PLASMA = 1000644167U;
         static const AkUniqueID PLAYER = 1069431850U;
         static const AkUniqueID QUEENXENOMORPH = 2982135181U;
