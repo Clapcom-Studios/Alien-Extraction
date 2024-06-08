e1c1238d11a5bee1931a14d80a3c46a72118292f
Eric <ericluque2003@gmail.com>
2024-06-08 12:42:24 +0200

    Fix Item Stash & Weapon

--- Ymir Engine/Game/Assets/Scripts/Item_Stash.cs
+++ Ymir Engine/Game/Assets/Scripts/Item_Stash.cs
@@ -54,6 +54,14 @@ public class Item_Stash : YmirComponent
                 player.ToggleMenu(true);
                 show_menu = false;
             }
+
+        }
+
+        if (other.Tag == "Player" && Input.IsGamepadButtonBPressedCS() || other.Name == "Player" && Input.IsGamepadButtonBPressedCS())
+        {
+            //TODO: Añadir timer al darle a la B
+            popup.SetActive(true);
+            show_menu = true;
         }
     }
     public void OnCollisionExit(GameObject other)
