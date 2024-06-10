577db57c4780431539ee4e220faad679355a0da0
NachoMoren <79315360+NachoMoren@users.noreply.github.com>
2024-06-10 19:18:25 +0200

    Corregido leak emitter image

--- Ymir Engine/Source/EmitterSetting.h
+++ Ymir Engine/Source/EmitterSetting.h
@@ -26,7 +26,7 @@ struct EmitterSetting //En Thor Engine esto seria el particle Module
 {
 public:
 	EmitterSetting();
-	~EmitterSetting();
+	virtual ~EmitterSetting();
 
 	EmitterType type;
 
@@ -278,6 +278,7 @@ struct EmitterColor : EmitterSetting
 struct EmitterImage : EmitterSetting
 {
 	EmitterImage();
+	~EmitterImage();
 	void Spawn(ParticleEmitter* emitter, Particle* particle);
 	void Update(float dt, ParticleEmitter* emitter);
 	void OnInspector();
