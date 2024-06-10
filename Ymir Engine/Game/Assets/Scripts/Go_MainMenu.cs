10e90013fd8cc774cbb34a0a39f0fafbfe936a1b
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2024-06-10 16:42:37 +0200

    Fix pause and death canvas

---
+++ Ymir Engine/Game/Assets/Scripts/Go_MainMenu.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+using YmirEngine;
+
+public class Go_MainMenu : YmirComponent
+{
+
+    private string sceneName = "UI/Scenes/StartScene";
+
+    // Loading scene
+    private GameObject loadSceneImg;
+    private bool loadScene = false;
+
+
+    private float finishTimer = 2f;
+    public bool saveGame = true;
+
+    public void Start()
+    {
+        loadSceneImg = InternalCalls.GetGameObjectByName("Loading Scene MainMenu");
+
+        if (loadSceneImg != null)
+        {
+            loadSceneImg.SetActive(false);
+        }
+
+        finishTimer = 2;
+        loadScene = false;
+    }
+
+    public void Update()
+    {
+        finishTimer -= Time.deltaTime;
+
+        if (loadScene)
+        {
+            loadSceneImg.SetActive(true);
+
+            if (finishTimer <= 0)
+            {
+                InternalCalls.LoadScene("Assets/" + sceneName + ".yscene");
+                loadScene = false;
+            }
+            return;
+        }
+    }
+
+    public void OnClickButton()
+    {
+        Audio.PauseAllAudios();
+
+        if (loadSceneImg != null)
+        {
+            loadSceneImg.SetActive(true);
+            loadScene = true;
+        }
+
+        if (saveGame)
+        {
+            Globals.GetPlayerScript().SavePlayer();
+        }
+    }
+}
